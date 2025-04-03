965245a6e5c18bf51b00abd836e4bf438a0bc836
anonimus228-luchiy <computerpc@gmail.com>
2025-04-03 19:26:29 +0600

    hw1

---
+++ shop_api/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from product.views import (
+    CategoryList, CategoryDetail,
+    ProductList, ProductDetail,
+    ReviewList, ReviewDetail
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/categories/', CategoryList.as_view()),
+    path('api/v1/categories/<int:pk>/', CategoryDetail.as_view()),
+    path('api/v1/products/', ProductList.as_view()),
+    path('api/v1/products/<int:pk>/', ProductDetail.as_view()),
+    path('api/v1/reviews/', ReviewList.as_view()),
+    path('api/v1/reviews/<int:pk>/', ReviewDetail.as_view()),
+]
