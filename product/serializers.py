e81e558d26bb3be3a2441baff8baa00516435af8
anonimus228-luchiy <computerpc@gmail.com>
2025-04-12 19:31:34 +0600

    hw4

--- product/serializers.py
+++ product/serializers.py
@@ -6,6 +6,17 @@ class ReviewSerializer(serializers.ModelSerializer):
         model = Review
         fields = '__all__'
 
+    def validate_stars(self, value):
+        if not (1 <= value <= 5):
+            raise serializers.ValidationError("Оценка должна быть от 1 до 5 звёзд.")
+        return value
+
+    def validate_text(self, value):
+        if len(value.strip()) < 5:
+            raise serializers.ValidationError("Текст отзыва слишком короткий.")
+        return value
+
+
 class ProductWithReviewsSerializer(serializers.ModelSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
     rating = serializers.FloatField()
@@ -14,10 +25,22 @@ class ProductWithReviewsSerializer(serializers.ModelSerializer):
         model = Product
         fields = ['id', 'title', 'description', 'price', 'category', 'reviews', 'rating']
 
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+    def validate_title(self, value):
+        if len(value.strip()) < 2:
+            raise serializers.ValidationError("Название фильма слишком короткое.")
+        return value
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Цена должна быть положительной.")
+        return value
+
 
 class CategorySerializer(serializers.ModelSerializer):
     products_count = serializers.IntegerField(read_only=True)
@@ -25,3 +48,8 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'products_count']
+
+    def validate_name(self, value):
+        if len(value.strip()) < 2:
+            raise serializers.ValidationError("Название категории должно содержать минимум 2 символа.")
+        return value
