81fba1a148550f89341806f52479f6ae34f48e90
anonimus228-luchiy <computerpc@gmail.com>
2025-04-19 17:18:22 +0600

    hw6

--- shop_api/urls.py
+++ shop_api/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('api/v1/categories/<int:pk>/', CategoryDetail.as_view()),
     path('api/v1/products/', ProductList.as_view()),
     path('api/v1/products/<int:pk>/', ProductDetail.as_view()),
-    path('api/v1/products/reviews/', ProductWithReviewsView.as_view()),  # <--- добавили
+    path('api/v1/products/reviews/', ProductWithReviewsView.as_view()),
     path('api/v1/reviews/', ReviewList.as_view()),
     path('api/v1/reviews/<int:pk>/', ReviewDetail.as_view()),
 ]
